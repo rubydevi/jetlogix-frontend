e2589648027c1721a38cbfb4c09db90e08dced4d
iamrubydevi <ariesabyss@gmail.com>
2023-11-02 15:43:13 +0530

    Dispatch the fetched aeroplanes

--- src/components/Aeroplanes.js
+++ src/components/Aeroplanes.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAeroplanes } from '../redux/aeroplanes/aeroplanesSplice';
 
-export default function Aeroplanes() {
+const Aeroplane = () => {
+  const dispatch = useDispatch();
+  const { aeroplanes, loading } = useSelector((store) => store.aeroplanes);
+
+  useEffect(() => {
+    dispatch(fetchAeroplanes());
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      Aeroplanes
-
+    <div className="container">
+      <h1 className="label">
+        Aeroplanes:
+      </h1>
+      <ul>
+        {aeroplanes.aeroplanes}
+      </ul>
     </div>
   );
-}
+};
+
+export default Aeroplane;
