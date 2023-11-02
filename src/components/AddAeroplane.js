4e70bcc826e1ae345a40fecf53cf3570f88eef49
RileyManda <rileymanda69@gmail.com>
2023-11-02 08:04:19 +0200

    Implemented add aeroplane form:ui

--- src/components/AddAeroplane.js
+++ src/components/AddAeroplane.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-export default function AddAeroplane() {
-  return (
-    <div>
-      AddAeroplane
+const AddAeroplane = () => (
+  <div>
+    <form className="row g-3">
+      <div className="col-md-12">
+        <input type="text" id="name" className="form-control" placeholder="Name" />
+      </div>
+      <div className="col-md-12">
+        <input type="file" className="form-control" id="image" />
+      </div>
+      <div className="col-md-12">
+        <input type="text" className="form-control" id="fee" placeholder="Enter the fee" />
+      </div>
+      <div className="col-md-12">
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+);
 
-    </div>
-  );
-}
+export default AddAeroplane;
--- src/components/styledcomponents/CenterContainer.js
+++ src/components/styledcomponents/CenterContainer.js
@@ -4,6 +4,7 @@ const CenteredDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 20px;
 `;
 
 export default CenteredDiv;
