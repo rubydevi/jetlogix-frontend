6d9cc4bd5b1f5a260637aa3f59a3f6da2650e8e2
RileyManda <rileymanda69@gmail.com>
2023-11-02 08:57:38 +0200

    Form:Updated input filed:Image upload and realigned form fields

--- src/components/AddAeroplane.js
+++ src/components/AddAeroplane.js
@@ -3,14 +3,16 @@ import React from 'react';
 const AddAeroplane = () => (
   <div>
     <form className="row g-3">
-      <div className="col-md-12">
+      <div className="col-md-12 mb-3">
         <input type="text" id="name" className="form-control" placeholder="Name" />
       </div>
-      <div className="col-md-12">
-        <input type="file" className="form-control" id="image" />
-      </div>
-      <div className="col-md-12">
-        <input type="text" className="form-control" id="fee" placeholder="Enter the fee" />
+      <div className="row mb-3">
+        <div className="col-md-6">
+          <input type="file" className="form-control" id="image" />
+        </div>
+        <div className="col-md-6">
+          <input type="number" className="form-control" id="fee" placeholder="Enter the fee" />
+        </div>
       </div>
       <div className="col-md-12">
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -18,5 +20,4 @@ const AddAeroplane = () => (
     </form>
   </div>
 );
-
 export default AddAeroplane;
