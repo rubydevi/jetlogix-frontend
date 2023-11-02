3988a611b9bc4f0f9446b679e48acb0825a3fb4b
Yonashenok <yonashenok24@gmail.com>
2023-11-02 09:23:36 +0300

    feat: create context provider for the autho

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AuthProvider } from './context/AuthProvider';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
@@ -8,10 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
