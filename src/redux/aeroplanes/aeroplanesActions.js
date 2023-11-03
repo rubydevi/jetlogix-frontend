afad038c8cdf231ed7082dfb25d586fe21fe8fa9
iamrubydevi <ariesabyss@gmail.com>
2023-11-03 16:10:39 +0530

    Make the fucntion accept user id as parameter

--- src/redux/aeroplanes/aeroplanesActions.js
+++ src/redux/aeroplanes/aeroplanesActions.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1/users/1/aeroplanes';
+const fetchAeroplanes = createAsyncThunk('aeroplanes/fetchAeroplanes', async (userId) => {
+  const url = `http://localhost:4000/api/v1/users/${userId}/aeroplanes`;
 
-const fetchAeroplanes = createAsyncThunk('aeroplanes/fetchAeroplanes', async () => {
   try {
     const response = await axios.get(url);
     return response.data;
