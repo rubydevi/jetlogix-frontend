d06a8e2e61eb9a84b406553d52aed1589303020e
iamrubydevi <ariesabyss@gmail.com>
2023-11-06 19:03:38 +0530

    Add delete function on Component side

--- src/components/DeleteAeroplane.js
+++ src/components/DeleteAeroplane.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAeroplanes, deleteAeroplane } from '../redux/aeroplanes/aeroplanesActions';
+import useAuth from '../hooks/useAuth';
 
-export default function DeleteAeroplane() {
+const DeleteAeroplane = () => {
+  const { auth } = useAuth();
+  const dispatch = useDispatch();
+  const aeroplaneStatus = useSelector((state) => state.aeroplanes.loading);
+  const aeroplanesData = useSelector((state) => state.aeroplanes.aeroplanes);
+
+  const { id } = auth;
+
+  useEffect(() => {
+    dispatch(fetchAeroplanes(id));
+  }, [dispatch, id]);
+
+  const handleDelete = (planeId) => {
+    if (aeroplaneStatus === false) {
+      dispatch(deleteAeroplane({ userId: id, aeroplaneId: planeId }));
+    }
+  };
+
   return (
-    <div>
-      DeleteAeroplane
+    <div className="d-flex align-item-center">
+      {aeroplanesData.aeroplanes && aeroplanesData.aeroplanes.length > 0 ? (
+        <table className="table table-borderless table-hover">
+          <thead>
+            <tr>
+              <th>Private Planes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {aeroplanesData.aeroplanes.map((aeroplane) => (
+              <tr key={aeroplane.id}>
+                <td>
+                  {aeroplane.name}
+                  {console.log(aeroplane.isRemoved)}
+                </td>
+                <td>
+
+                  <button
+                    type="submit"
+                    onClick={() => handleDelete(aeroplane.id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div>No aeroplanes yet</div>
+      )}
     </div>
   );
-}
+};
+
+export default DeleteAeroplane;
