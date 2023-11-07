04a02fa2998a6613471b88ce068c6a0b26731578
RileyManda <rileymanda69@gmail.com>
2023-11-07 20:30:10 +0200

    Form validations:UI

--- src/components/AddAeroplane.js
+++ src/components/AddAeroplane.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { BiAlarm } from 'react-icons/bi';
 import { createAeroplane } from '../redux/aeroplanes/aeroplanesActions';
 import AuthContext from '../context/AuthProvider';
-import AeroplaneForm from './AddAeroplaneForm';
+import AddAeroplaneForm from './AddAeroplaneForm';
 
 const AddAeroplane = () => {
   const { auth } = useContext(AuthContext);
@@ -20,7 +21,7 @@ const AddAeroplane = () => {
 
   const dispatch = useDispatch();
   const history = useNavigate();
-
+  const [error, setError] = useState(null);
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -28,18 +29,47 @@ const AddAeroplane = () => {
       if (response.payload && !response.error) {
         history('/aeroplanes');
       } else {
-        console.error('Error or invalid response');
+        setError('Error or invalid response');
+        const liveToast = new window.bootstrap.Toast(document.getElementById('liveToast'));
+        liveToast.show();
       }
     } catch (error) {
-      console.error('Error:', error);
+      setError(`Error: ${error.message}`);
+      const liveToast = new window.bootstrap.Toast(document.getElementById('liveToast'));
+      liveToast.show();
     }
   };
+
   return (
-    <AeroplaneForm
-      aeroplane={aeroplane}
-      setAeroplane={setAeroplane}
-      handleSubmit={handleSubmit}
-    />
+    <div className="container">
+      <div className="toast-container position-fixed top-0 end-0 p-3">
+        <div id="liveToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+          <div className="toast-header">
+            <div className="float-sm-start">
+              <BiAlarm size={20} className="me-2" />
+              <i className="bi bi-react me-2" />
+            </div>
+
+            <strong className="me-auto">Alert</strong>
+            <div className="float-sm-end">
+              <button
+                type="button"
+                className="btn-close me-auto"
+                data-bs-dismiss="toast"
+                aria-label="Close"
+                id="toastClose"
+              />
+            </div>
+          </div>
+          <div className="toast-body">{error}</div>
+        </div>
+      </div>
+      <AddAeroplaneForm
+        aeroplane={aeroplane}
+        setAeroplane={setAeroplane}
+        handleSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
--- src/components/AddAeroplaneForm.js
+++ src/components/AddAeroplaneForm.js
@@ -1,110 +1,115 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AeroplaneForm = ({ aeroplane, setAeroplane, handleSubmit }) => (
-  <div className="container">
-    <form onSubmit={handleSubmit}>
-      <div className="row">
-        <div className="col-md-6 mb-3">
-          <input
-            type="text"
-            id="name"
-            className="form-control"
-            placeholder="Name"
-            value={aeroplane.name}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              name: e.target.value,
-            })}
-          />
-        </div>
-        <div className="col-md-6 mb-3">
-          <input
-            type="text"
-            id="model"
-            className="form-control"
-            placeholder="Model"
-            value={aeroplane.model}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              model: e.target.value,
-            })}
-          />
+const AeroplaneForm = ({ aeroplane, setAeroplane, handleSubmit }) => {
+  const handleInputChange = (e, field) => {
+    setAeroplane({
+      ...aeroplane,
+      [field]: e.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      handleSubmit(event);
+    }
+
+    form.classList.add('was-validated');
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleFormSubmit}>
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <input
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="Name"
+              value={aeroplane.name}
+              onChange={(e) => handleInputChange(e, 'name')}
+              required
+            />
+
+            <div className="invalid-feedback">Please provide a valid name.</div>
+          </div>
+          <div className="col-md-6 mb-3">
+            <input
+              type="text"
+              id="model"
+              className="form-control"
+              placeholder="Model"
+              value={aeroplane.model}
+              onChange={(e) => handleInputChange(e, 'model')}
+            />
+          </div>
+          <div className="col-12 mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Image URL"
+              id="image"
+              value={aeroplane.image}
+              onChange={(e) => handleInputChange(e, 'image')}
+            />
+          </div>
+          <div className="col-12 mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Description"
+              id="description"
+              value={aeroplane.description}
+              onChange={(e) => handleInputChange(e, 'description')}
+            />
+          </div>
+          <div className="col-md-6 mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="number_of_seats"
+              placeholder="Number of seats"
+              value={aeroplane.number_of_seats}
+              onChange={(e) => handleInputChange(e, 'number_of_seats')}
+            />
+          </div>
+          <div className="col-md-6 mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="location"
+              placeholder="Location"
+              value={aeroplane.location}
+              onChange={(e) => handleInputChange(e, 'location')}
+            />
+          </div>
+          <div className="col-md-6 mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="fee"
+              placeholder={aeroplane.fee === 0 ? 'Enter fee' : ''}
+              value={aeroplane.fee !== 0 ? aeroplane.fee : ''}
+              onChange={(e) => handleInputChange(e, 'fee')}
+            />
+          </div>
+
         </div>
         <div className="col-12 mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Image URL"
-            id="image"
-            value={aeroplane.image}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              image: e.target.value,
-            })}
-          />
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </div>
-        <div className="col-12 mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Description"
-            id="description"
-            value={aeroplane.description}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              description: e.target.value,
-            })}
-          />
-        </div>
-        <div className="col-md-6 mb-3">
-          <input
-            type="number"
-            className="form-control"
-            id="number_of_seats"
-            placeholder="Number of seats"
-            value={aeroplane.number_of_seats}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              number_of_seats: e.target.value,
-            })}
-          />
-        </div>
-        <div className="col-md-6 mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="location"
-            placeholder="Location"
-            value={aeroplane.location}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              location: e.target.value,
-            })}
-          />
-        </div>
-        <div className="col-md-6 mb-3">
-          <input
-            type="number"
-            className="form-control"
-            id="fee"
-            placeholder="Enter the fee"
-            value={aeroplane.fee}
-            onChange={(e) => setAeroplane({
-              ...aeroplane,
-              fee: +e.target.value,
-            })}
-          />
-        </div>
-      </div>
-      <div className="col-12 mb-3">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
+};
 
 AeroplaneForm.propTypes = {
   aeroplane: PropTypes.shape({
