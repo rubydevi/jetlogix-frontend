8ac9039c92cd3f2e7088b32b8774e9fc0ee3a0d5
RileyManda <rileymanda69@gmail.com>
2023-11-08 13:28:10 +0200

    Iterations:Responsive layout reservation card

--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -7,61 +7,58 @@ import {
 } from './styledcomponents/ReservationStyles';
 
 const ReservationCard = ({ aeroplane }) => (
-  <div>
-    <CustomCard>
-      <div className="card-body">
-        <CardTitle>Reservation Details</CardTitle>
-        <CardContent>
-          <CardText>
-            <div className="text-left"><strong>Name:</strong></div>
-            {' '}
-            <small>{aeroplane.name}</small>
-          </CardText>
-          <CardText>
-            <div className="text-left"><strong>Date:</strong></div>
-            {' '}
-            <small>
-              {' '}
-              {aeroplane.reserved_date}
-            </small>
-          </CardText>
-          <CardText>
-            <strong>Start Time:</strong>
-            {' '}
-            <small>
-              {' '}
-              {aeroplane.start_time}
-              {' '}
-            </small>
-          </CardText>
-          <CardText>
-            <strong>End Time:</strong>
-            {' '}
-            <small>
-              {' '}
-              {aeroplane.end_time}
-              {' '}
-            </small>
-          </CardText>
-          <CardText>
-            <strong>Start Location:</strong>
-            {' '}
-            <small>
-              {' '}
-              {aeroplane.start_location}
-            </small>
-          </CardText>
-          <CardText>
-            <strong>Destination:</strong>
-            {' '}
-            <small>
-              {' '}
-              {aeroplane.destination}
-            </small>
-          </CardText>
-        </CardContent>
-      </div>
-    </CustomCard>
+  <div className="row">
+    <div className="col-md-6">
+      <CustomCard className="card">
+        <div className="card-body">
+          <CardTitle className="card-title">Reservation Details</CardTitle>
+          <CardContent>
+            <CardText className="card-text">
+              <div>
+                <strong>Name:</strong>
+                {' '}
+                <small>{aeroplane.name}</small>
+              </div>
+            </CardText>
+            <CardText className="card-text">
+              <div>
+                <strong>Date:</strong>
+                {' '}
+                <small>{aeroplane.reserved_date}</small>
+              </div>
+            </CardText>
+            <CardText className="card-text">
+              <div>
+                <strong>Start Time:</strong>
+                {' '}
+                <small>{aeroplane.start_time}</small>
+              </div>
+            </CardText>
+            <CardText className="card-text">
+              <div>
+                <strong>End Time:</strong>
+                {' '}
+                <small>{aeroplane.end_time}</small>
+              </div>
+            </CardText>
+            <CardText className="card-text">
+              <div>
+                <strong>Start Location:</strong>
+                {' '}
+                <small>{aeroplane.start_location}</small>
+              </div>
+            </CardText>
+            <CardText className="card-text">
+              <div>
+                <strong>Destination:</strong>
+                {' '}
+                <small>{aeroplane.destination}</small>
+              </div>
+            </CardText>
+          </CardContent>
+        </div>
+      </CustomCard>
+    </div>
   </div>
 );
 
