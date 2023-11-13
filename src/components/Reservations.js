293a55b6c81a00ac6feb9510a7831a3c9dbfd194
Yonashenok <yonashenok24@gmail.com>
2023-11-13 09:44:15 +0300

    feat: loading skelton for reservation

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -2,25 +2,26 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservedAeroplanes } from '../redux/aeroplanes/aeroplanesActions';
 import ReservationCard from './ReservationCard';
+import ReserveCardLoading from './ReserveCardLoading';
 
 const Reservations = () => {
   const dispatch = useDispatch();
   const userAeroplanes = useSelector((state) => state.reservations.reservation);
-  const loading = useSelector((state) => state.aeroplanes.reservedAeroplanesLoading);
-  const error = useSelector((state) => state.aeroplanes.reservedAeroplanesError);
+  const loading = useSelector((state) => state.reservations.loading);
+  const error = useSelector(
+    (state) => state.aeroplanes.reservedAeroplanesError,
+  );
   useEffect(() => {
     dispatch(fetchReservedAeroplanes());
   }, [dispatch]);
 
   if (loading) {
-    return <p>Loading</p>;
+    return <ReserveCardLoading />;
   }
 
   if (error) {
     return (
-
       <div className="d-flex justify-content-center align-items-center">
-
         <p>Something went wrong</p>
       </div>
     );
@@ -28,7 +29,11 @@ const Reservations = () => {
 
   const aeroplaneCards = userAeroplanes.map((aeroplane, index) => (
     <div className="col-lg-6 col-12 mb-4" key={aeroplane.id}>
-      <ReservationCard aeroplane={aeroplane} index={index} style={{ marginRight: '40px' }} />
+      <ReservationCard
+        aeroplane={aeroplane}
+        index={index}
+        style={{ marginRight: '40px' }}
+      />
     </div>
   ));
 
@@ -38,9 +43,7 @@ const Reservations = () => {
         <h3 className="fw-normal"> Reservations</h3>
       </div>
       {userAeroplanes && userAeroplanes.length > 0 ? (
-        <div className="row">
-          { aeroplaneCards }
-        </div>
+        <div className="row">{aeroplaneCards}</div>
       ) : (
         <div className="d-flex justify-content-center align-items-center">
           <p>No reserved aeroplanes found.</p>
---
+++ src/components/ReserveCardLoading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReservationCardLoading from '../ui/reservationCardLoading';
+
+const ReserveCardLoading = () => (
+  <div className="row container">
+    <div className="col-lg-6 col-12 mb-4">
+      <ReservationCardLoading />
+    </div>
+    <div className="col-lg-6 col-12 mb-4">
+      <ReservationCardLoading />
+    </div>
+    <div className="col-lg-6 col-12 mb-4">
+      <ReservationCardLoading />
+    </div>
+    <div className="col-lg-6 col-12 mb-4">
+      <ReservationCardLoading />
+    </div>
+  </div>
+);
+
+export default ReserveCardLoading;
