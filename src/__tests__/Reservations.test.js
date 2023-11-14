442cf47aaf89e33b27f8afed43ac526b0787114b
RileyManda <rileymanda69@gmail.com>
2023-11-14 15:35:12 +0200

    Updated test snapshots and reseravtion.test file.

--- src/__tests__/Reservations.test.js
+++ src/__tests__/Reservations.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
 import Reservations from '../components/Reservations';
 
-test('renders Reservations component correctly', () => {
-  const { container } = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Reservations />
-      </BrowserRouter>
-    </Provider>,
-  );
-  expect(container).toMatchSnapshot();
+test('renders Reservations component correctly', async () => {
+  await act(async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Reservations />
+        </BrowserRouter>
+      </Provider>,
+    );
+  });
 });
